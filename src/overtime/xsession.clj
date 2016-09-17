dbeed2249ca0c7ce64677d312d176af01a1cf4d3
stuart <stuartj@berwickheights.com>
2016-09-17 15:41:44 -0400

    Refactored xsession namespace to use given event handlers instead of given multimethod impls

--- src/overtime/xsession.clj
+++ src/overtime/xsession.clj
@@ -6,89 +6,79 @@
 ;(ot/event-debug-on)
 ;(ot/event-debug-off)
 
-(def xsession-map {24 :1-left
-                   25 :2-left
-                   26 :3-left
-                   27 :high-left
-                   28 :mid-left
-                   29 :low-left
-                   12 :pitch-left
-                   11 :volume-left
+(defonce ^:private handlers-per-knob (atom {}))
 
-                   44 :headphone-left
-                   46 :rewind-left
-                   43 :ff-left
-                   58 :cue-left
-                   70 :play-left
 
-                   31 :1-right
-                   32 :2-right
-                   33 :3-right
-                   34 :high-right
-                   35 :mid-right
-                   36 :low-right
-                   15 :pitch-right
-                   14 :volume-right
+(defonce ^:private xsession-map {24 :1-left
+                                 25 :2-left
+                                 26 :3-left
+                                 27 :high-left
+                                 28 :mid-left
+                                 29 :low-left
+                                 12 :pitch-left
+                                 11 :volume-left
 
-                   45 :headphone-right
-                   56 :rewind-right
-                   57 :ff-right
-                   59 :cue-right
-                   69 :play-right
+                                 44 :headphone-left
+                                 46 :rewind-left
+                                 43 :ff-left
+                                 58 :cue-left
+                                 70 :play-left
 
-                   17 :fader
-                   20 :inv-fader})
+                                 31 :1-right
+                                 32 :2-right
+                                 33 :3-right
+                                 34 :high-right
+                                 35 :mid-right
+                                 36 :low-right
+                                 15 :pitch-right
+                                 14 :volume-right
 
-(defmulti on-event (fn [{note :note}] (get xsession-map note :unmapped)))
-(defmethod on-event :default [{note :note velocity :velocity}] (println "Unknown MIDI event:" note ", velocity:" velocity))
-(defmethod on-event :1-left [{velocity :velocity}] (println "1 left:" velocity))
-(defmethod on-event :2-left [{velocity :velocity}] (println "2 left:" velocity))
-(defmethod on-event :3-left [{velocity :velocity}] (println "3 left:" velocity))
-(defmethod on-event :high-left [{velocity :velocity}] (println "High left:" velocity))
-(defmethod on-event :mid-left [{velocity :velocity}] (println "Mid left:" velocity))
-(defmethod on-event :low-left [{velocity :velocity}] (println "Low left:" velocity))
-(defmethod on-event :pitch-left [{velocity :velocity}] (println "Pitch left:" velocity))
-(defmethod on-event :volume-left [{velocity :velocity}] (println "Volume left:" velocity))
+                                 45 :headphone-right
+                                 56 :rewind-right
+                                 57 :ff-right
+                                 59 :cue-right
+                                 69 :play-right
 
-(defmethod on-event :1-right [{velocity :velocity}] (println "1 right:" velocity))
-(defmethod on-event :2-right [{velocity :velocity}] (println "2 right:" velocity))
-(defmethod on-event :3-right [{velocity :velocity}] (println "3 right:" velocity))
-(defmethod on-event :high-right [{velocity :velocity}] (println "High right:" velocity))
-(defmethod on-event :mid-right [{velocity :velocity}] (println "Mid right:" velocity))
-(defmethod on-event :low-right [{velocity :velocity}] (println "Low right:" velocity))
-(defmethod on-event :pitch-right [{velocity :velocity}] (println "Pitch right:" velocity))
-(defmethod on-event :volume-right [{velocity :velocity}] (println "Volume right:" velocity))
+                                 17 :fader
+                                 20 :inv-fader})
 
-(defmethod on-event :headphone-left [_event] (println "Headphone left"))
-(defmethod on-event :rewind-left [_event] (println "Rewind left"))
-(defmethod on-event :ff-left [_event] (println "FF left"))
-(defmethod on-event :cue-left [_event] (println "Cue left"))
-(defmethod on-event :play-left [_event] (println "Play left"))
+(defn- get-midi-note
+  [event]
+  (let [midi-note (-> (:note event)
+                      xsession-map)]
+    (if (nil? midi-note)
+      (throw (str "Unknown XSession MIDI note:" midi-note))
+      midi-note)))
 
-(defmethod on-event :fader [{velocity :velocity}] (println "Fade:" velocity))
-(defmethod on-event :inv-fader [{velocity :velocity}] (println "Inverted Fade:" velocity))
-
-(defmethod on-event :headphone-right [_event] (println "Headphone right"))
-(defmethod on-event :rewind-right [_event] (println "Rewind right"))
-(defmethod on-event :ff-right [_event] (println "FF right"))
-(defmethod on-event :cue-right [_event] (println "Cue right"))
-(defmethod on-event :play-right [_event] (println "Play right"))
-
-(defn- set-event-hdlr
+(defn- set-midi-event-hdlr
   [type hdlr-key]
   (ot/on-event
     [:midi type]
-    (fn [event] (on-event event))
-    hdlr-key)
-  )
-(defn start [] (map set-event-hdlr [:control-change :note-on] [::cc-hdlr ::note-hdlr]))
+    (fn [event]
+      (let [handlers ((get-midi-note event) @handlers-per-knob)]
+        (doseq [[_k f] handlers] (f (:velocity event)))))
+    hdlr-key))
+
+(defn start [] (map set-midi-event-hdlr [:control-change :note-on] [::cc-hdlr ::note-hdlr]))
 (defn stop [] (map ot/remove-event-handler [::cc-hdlr ::note-hdlr]))
 
+(defn add-handler!
+  [xs-event key f]
+  (swap! handlers-per-knob update-in [xs-event] assoc key f))
+
+(defn remove-handler!
+  [xs-event key]
+  (swap! handlers-per-knob update-in [xs-event] dissoc key))
+
 (comment
-  (defmethod on-event :1-left [{velocity :velocity}] (println "Density:" velocity))
-  (defmethod on-event :high-left [{velocity :velocity}] (println "Grain Dur:" velocity))
-  (defmethod on-event :2-left [{velocity :velocity}] (println "Freq:" velocity))
-  (defmethod on-event :mid-left [{velocity :velocity}] (println "Freq Dev Noise:" velocity))
-  (defmethod on-event :3-left [{velocity :velocity}] (println "Mod Freq:" velocity))
-  (defmethod on-event :volume-left [{velocity :velocity}] (println "Amp:" velocity))
-  )
+  (start)
+  (stop)
+  (add-handler! :1-left :foo #(println "1-left:" %))
+  (add-handler! :1-left :foo #(println "1-left other:" %))
+  (add-handler! :1-left :bar #(println "1-left bar:" %))
+  (remove-handler! :1-left :bar)
+  (add-handler! :high-left :foo #(println "high-left:" %))
+  (add-handler! :2-left :foo #(println "2-left:" %))
+  (add-handler! :mid-left :foo #(println "mid-left:" %))
+  (add-handler! :3-left :foo #(println "3-left:" %))
+  (add-handler! :volume-left :foo #(println "volume-left:" %)))
