8e24e00a0dc9289c728fe6631b45d3b9737a52f7
stuart <stuartj@berwickheights.com>
2016-09-27 09:21:06 -0400

    Added log4j to project

--- project.clj
+++ project.clj
@@ -7,4 +7,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [overtone "0.10.1"]
                  [seesaw "1.4.5"]
-                 [org.clojure/tools.logging "0.3.1"]])
+                 [org.clojure/tools.logging "0.3.1"]
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jmdk/jmxtools
+                                                    com.sun.jmx/jmxri]]])
