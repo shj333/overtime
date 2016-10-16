94984b659df482cc321c983821824d357ec76d3c
stuart <stuartj@berwickheights.com>
2016-10-16 12:41:55 -0400

    midi-controller-gui: added ability to specify play and stop keys on midi device

--- src/overtime/midi_controller_gui.clj
+++ src/overtime/midi_controller_gui.clj
@@ -7,9 +7,9 @@
 
 
 (defn- init-xs-hdlrs
-  [frame-key & {:keys [device instr-type instr sliders] :or {sliders []}}]
-  (mc/add-handler! device :play-left frame-key (fn [_x] (instr/play-sound-at (+ (ot/now) 500) instr-type instr)))
-  (mc/add-handler! device :cue-left frame-key (fn [_x] (instr/stop-sound-at (+ (ot/now) 500) instr-type instr)))
+  [frame-key & {:keys [instr-type instr device sliders play stop] :or {sliders []}}]
+  (mc/add-handler! device play frame-key (fn [_x] (instr/play-sound-at (+ (ot/now) 500) instr-type instr)))
+  (mc/add-handler! device stop frame-key (fn [_x] (instr/stop-sound-at (+ (ot/now) 500) instr-type instr)))
   (doseq [[slider-key {:keys [knob-key min max slider-f] :or {slider-f u/num-lin-lin}}] sliders]
     (mc/add-handler! device knob-key frame-key #(gui/change-slider-val frame-key slider-key (slider-f % 0 127 min max)))))
 
